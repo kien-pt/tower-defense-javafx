63b2ca8ef3635b2d6fc5344c3c3e5aceae262087
khanhnguyenuet <50212197+khanhnguyenuet@users.noreply.github.com>
2019-10-26 13:19:51 +0700

    ban dc r

--- src/game/object/Bullet.java
+++ src/game/object/Bullet.java
@@ -44,11 +44,33 @@ public class Bullet extends GameObject {
 
     public void MoveBullet(int targetX, int targetY) {
         int distance = (int) Math.sqrt(Math.pow((double) targetX - posX, 2) + Math.pow((double) targetY - posY, 2));
-        if (isFired == true) {
-            this.posX += ((targetX - posX) / (distance / 5));
-            this.posY += ((targetY - posY) / (distance / 5));
-            if (this.posX >= targetX || this.posY >= targetY)
-                isFired = false;
+        distance /= 5;
+        if (this.IsFired()) {
+            if (distance != 0) {
+                if (targetX > posX && targetY > posY) {
+                    this.posX += (targetX - posX) / distance;
+                    this.posY += (targetY - posY) / distance;
+                } else if (targetX < posX && targetY > posY) {
+                    this.posX -= (posX - targetX) / distance;
+                    this.posY += (targetY - posY) / distance;
+                } else if (targetX < posX && targetY < posY) {
+                    this.posX -= (posX - targetX) / distance;
+                    this.posY -= (posY - targetY) / distance;
+                } else if (targetX > posX && targetY < posY) {
+                    this.posX += (targetX - posX) / distance;
+                    this.posY -= (posY - targetY) / distance;
+                } else if (targetX == posX && targetY > posY) {
+                    this.posY += (targetY - posY) / distance;
+                } else if (targetX == posX && targetY < posY) {
+                    this.posY -= (posY - targetY) / distance;
+                } else if (targetX < posX && targetY == posY) {
+                    this.posX -= (posX - targetX) / distance;
+                } else if (targetX > posX && targetY == posY) {
+                    this.posX += (targetX - posX) / distance;
+                }
+            } else {
+                this.setFired(false);
+            }
         }
     }
 }
--- src/game/tower/NormalTower.java
+++ src/game/tower/NormalTower.java
@@ -1,20 +1,74 @@
 package game.tower;
 
+import game.enemy.NormalEnemy;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import game.troops.Bullet;
 
+import java.util.ArrayList;
+
 public class NormalTower extends ActiveTower {
-    private Bullet bullet;
+    private ArrayList<Bullet> bullets = new ArrayList<>();
+    private ArrayList<NormalEnemy> targets = new ArrayList<>();
+    private ArrayList<NormalEnemy> enemies = new ArrayList<>();
+    private double range;
     public NormalTower(int posX, int posY) {
         super(posX, posY, new Image("file:resources/tower/normal_tower.png"));
-        this.bullet = new Bullet(this.posX + this.getWidth() / 2, this.posY + this.getHeight() / 2, 5, 5, true);
+        this.range = 600;
+        for (int i = 0; i < 5; i++)
+            this.enemies.add(new NormalEnemy(400 - i * 10, 100 + i * 100));
+        CreateTarget(this.enemies);
+        //while (!this.targets.isEmpty())
+        this.bullets.add(new Bullet(this.posX + this.getWidth() / 2, this.posY + this.getHeight() / 2, 5, 5, true));
+        this.bullets.add(new Bullet(this.posX + this.getWidth() / 2, this.posY + this.getHeight() / 2, 5, 5, true));
+        this.bullets.add(new Bullet(this.posX + this.getWidth() / 2, this.posY + this.getHeight() / 2, 5, 5, true));
+        this.bullets.add(new Bullet(this.posX + this.getWidth() / 2, this.posY + this.getHeight() / 2, 5, 5, true));
+        this.bullets.add(new Bullet(this.posX + this.getWidth() / 2, this.posY + this.getHeight() / 2, 5, 5, true));
+        this.bullets.add(new Bullet(this.posX + this.getWidth() / 2, this.posY + this.getHeight() / 2, 5, 5, true));
+    }
+
+    public double getRange() {
+        return this.range;
     }
 
     @Override
     public void draw(GraphicsContext gc) {
         super.draw(gc);
-        bullet.draw(gc);
-        bullet.MoveBullet(500, 500);
+        for (NormalEnemy i : this.enemies)
+            i.draw(gc);
+        for (Bullet i : bullets)
+            i.draw(gc);
+        /*while (!this.targets.isEmpty()){
+            this.bullets = new Bullet(this.posX + this.getWidth() / 2, this.posY + this.getHeight() / 2, 5, 5, true);
+            //this.bullets.draw(gc);
+            this.bullets.MoveBullet(this.targets.get(0).getPosX(),this.targets.get(0).getPosY());
+            this.targets.remove(0);
+        }*/
+        if (!this.bullets.isEmpty() && !this.targets.isEmpty()) {
+            this.bullets.get(0).MoveBullet(this.targets.get(0).getPosX(), this.targets.get(0).getPosY());
+            if (this.bullets.get(0).getPosX() == this.targets.get(0).getPosX() && this.bullets.get(0).getPosY() == this.targets.get(0).getPosY()) {
+                this.bullets.remove(0);
+                this.targets.remove(0);
+            }
+        }
+    }
+
+    public void CreateBullet() {
+
+    }
+
+    public void CreateTarget(ArrayList<NormalEnemy> enemies) {
+        for (int i = 0; i < enemies.size(); i++) {
+            double distance = Math.sqrt(Math.pow(this.posX - enemies.get(i).getPosX(), 2) + Math.pow(this.posY - enemies.get(i).getPosY(), 2));
+            if (distance <= this.getRange()) {
+                this.targets.add(enemies.get(i));
+            }
+        }
+    }
+
+    public void Shoot() {
+        if (!this.targets.isEmpty()) {
+            //this.bullet = new Bullet(this.posX + this.getWidth() / 2, this.posY + this.getHeight() / 2, 5, 5, true);
+        }
     }
 }
